1c49a5282726457e865f8cc72ce8940795a24db4
Quan guanyu <quanguanyu@qq.com>
2021-12-22 09:42:56 +0800

    调整了系统窗口优先级

--- tomato.go
+++ tomato.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/lxn/walk"
+	"github.com/lxn/win"
 	"time"
 )
 
@@ -12,9 +13,11 @@ func RunTimer() {
 	for {
 		time.Sleep(sitDuration)
 		fmt.Println("请站起来吧")
-		walk.MsgBox(wmain, "请站起来吧", "你已经久坐了35分钟", walk.MsgBoxTopMost|walk.MsgBoxOK|walk.MsgBoxIconWarning)
+
+		_ = tray.ShowCustom("请站起来吧", "你已经久坐了35分钟", ico)
+		walk.MsgBox(wmain, "请站起来吧", "你已经久坐了35分钟", win.MB_SYSTEMMODAL|walk.MsgBoxOK|walk.MsgBoxIconWarning)
 		time.Sleep(standDuration)
-		fmt.Println("请坐下")
-		walk.MsgBox(wmain, "请坐下", "你已经站立了5分钟", walk.MsgBoxTopMost|walk.MsgBoxOK|walk.MsgBoxIconWarning)
+		_ = tray.ShowCustom("请坐下", "你已经站立了5分钟", ico)
+		walk.MsgBox(wmain, "请坐下", "你已经站立了5分钟", win.MB_SYSTEMMODAL|walk.MsgBoxOK|walk.MsgBoxIconWarning)
 	}
 }
