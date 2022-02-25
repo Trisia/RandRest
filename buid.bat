2eafe2bd8afdde15584ea899cf92fa1e12ef2ec8
cliven <quanguanyu@qq.com>
2022-02-25 19:11:41 +0800

    交叉编译脚本

--- buid.bat
+++ buid.bat
@@ -1,3 +1,10 @@
 MD target
+@echo off
 
+SET GOARCH=386
+go build -ldflags="-s -w -H windowsgui" -o "target\StandRest_386.exe"
+echo [+] target\StandRest_386.exe
+
+SET GOARCH=amd64
 go build -ldflags="-s -w -H windowsgui" -o "target\StandRest.exe"
+echo [+] target\StandRest.exe
